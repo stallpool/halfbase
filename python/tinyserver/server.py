978954cd9393e806c450a4acef46fa2182a389db
Seven Lju <dna2gle@gmail.com>
2021-12-16 20:16:36 +0800

    tinyserver-py: add for python3
    - move previous server.py to py2server.py
    - add python3 version of server.py

--- python/tinyserver/server.py
+++ python/tinyserver/server.py
@@ -1,48 +1,15 @@
-#!/usr/bin/env python
-"""
-Very simple HTTP server in python.
-Usage::
-    ./server.py [<port>]
-Send a GET request::
-    curl http://localhost
-Send a HEAD request::
-    curl -I http://localhost
-Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
-"""
+import http.server
 
-import json
-import os
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
-class S(BaseHTTPRequestHandler):
-
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write("<html><body><h1>GET</h1></body></html>")
-
-    def do_HEAD(self):
-        self._set_headers()
-
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        self._set_headers()
-        self.wfile.write(json.dumps({ 'hello': 'POST' }))
-
-def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
-    httpd.serve_forever()
+class Handler(http.server.SimpleHTTPRequestHandler) :
+   def do_GET(s) :
+      print('GET %s --> %s)' % (s.path, s.client_address))
+      print(s.headers)
+      super(Handler, s).do_GET()
 
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+   from sys import argv
+   port = 8080
+   if len(argv) == 2:
+       port = int(argv[1])
+   s = http.server.HTTPServer( ('', port), Handler )
+   s.serve_forever()
