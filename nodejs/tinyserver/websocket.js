cd172e8335703a369a0b6c4ca8eff5ae346d2eb6
Seven Lju <dna2gle@gmail.com>
2024-04-24 11:38:59 +0800

    [nodejs/tinyserver] bugfix for websocket
    - should work with multiple entries

--- nodejs/tinyserver/websocket.js
+++ nodejs/tinyserver/websocket.js
@@ -1,3 +1,4 @@
+// version 1.0.1
 const i_ws = require('ws'); // require `npm install ws`
 
 /*
@@ -30,6 +31,60 @@ function delWsPathHandler(path) {
    delete env.pathConfig[path];
 }
 
+function onConnection(ws, req, path, config) {
+   if (!path || !config) {
+      try { ws.terminate(); } catch (err) { }
+      return;
+   }
+   const fn = config.fn;
+   const onOpen = config.opt.onOpen;
+   const onClose = config.opt.onClose;
+   const onError = config.opt.onError;
+   const timeout = config.opt.timeout;
+   const local = { ws };
+   let timer = 0;
+   onOpen && onOpen(ws, local);
+   if (timeout > 0) timer = setTimeout(() => {
+      if (local.authenticated) return;
+      try { ws.terminate(); } catch(_) { }
+   }, timeout);
+   ws.on('close', () => {
+      local.closed = true;
+      if (timer) clearTimeout(timer);
+      onClose && onClose(ws, local);
+   });
+   ws.on('error', (err) => {
+      local.closed = true;
+      onError && onError(err, ws, local);
+   });
+   ws.on('message', (m) => {
+      try {
+         if (!m.length || m.length > 10*1024*1024 /* 10MB */) throw 'invalid message';
+         m = JSON.parse(m);
+         fn && fn(ws, local, m);
+      } catch(err) {
+         try { ws.terminate(); } catch(_) { }
+         return;
+      }
+   });
+}
+
+function onUpgrade(req, socket, head) {
+   // authenticate: socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n'); socket.destroy();
+   const configs = Object.values(env.pathConfig);
+   for (const config of configs) {
+      if (!req.url.startsWith(config.path)) continue;
+      const rpath = req.url.substring(config.path.length);
+      if (rpath && rpath.charAt(0) !== '/') continue;
+      env.wss.handleUpgrade(req, socket, head, (ws) => {
+         env.wss.emit('connection', ws, req, rpath, config);
+      });
+      return;
+   }
+   socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+   socket.destroy();
+}
+
 function addWsPathHandler(server, path, fn, opt) {
    if (!env.pathConfig[path]) env.pathConfig[path] = {};
    const config = env.pathConfig[path];
@@ -39,51 +94,8 @@ function addWsPathHandler(server, path, fn, opt) {
    if (env.wss) return;
    // lazy init
    env.wss = new i_ws.WebSocketServer({ noServer: true });
-   env.wss.on('connection', (ws, req, path) => {
-      const config = env.pathConfig[path];
-      if (!path || !config) {
-         try { ws.terminate(); } catch (err) { }
-         return;
-      }
-      const fn = config.fn;
-      const onOpen = config.opt.onOpen;
-      const onClose = config.opt.onClose;
-      const onError = config.opt.onError;
-      const timeout = config.opt.timeout;
-      const local = { ws };
-      onOpen && onOpen(ws, local);
-      if (timeout > 0) setTimeout(() => (!local.authenticated) && ws.terminate(), timeout);
-      ws.on('close', () => {
-         local.closed = true;
-         onClose && onClose(ws, local);
-      });
-      ws.on('error', (err) => {
-         local.closed = true;
-         onError && onError(err, ws, local);
-      });
-      ws.on('message', (m) => {
-         try {
-            if (!m.length || m.length > 10*1024*1024 /* 10MB */) throw 'invalid message';
-            m = JSON.parse(m);
-            fn && fn(ws, local, m);
-         } catch(err) {
-            ws.terminate();
-            return;
-         }
-      });
-   });
-   server.on('upgrade', (req, socket, head) => {
-      // authenticate: socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n'); socket.destroy();
-      const configs = Object.values(env.pathConfig);
-      for (const config of configs) {
-         env.wss.handleUpgrade(req, socket, head, (ws) => {
-            env.wss.emit('connection', ws, req, config.path);
-         });
-         return;
-      }
-      socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-      socket.destroy();
-   });
+   env.wss.on('connection', onConnection);
+   server.on('upgrade', onUpgrade);
 }
 
 function makeWebsocket(server, name, path, fn, opt) {
@@ -96,6 +108,19 @@ function makeWebsocket(server, name, path, fn, opt) {
    addWsPathHandler(server, path, fn, opt);
 }
 
+function keepalive(ws, local, ms) {
+   ms = ms || 15000;
+   if (ws.readyState !== 1) return;
+   try { ws.ping(); } catch(err) {}
+   setTimeout(keepalive, ms, ws, local, ms);
+}
+
+function safeSendJson(ws, json) {
+   try { ws.send(JSON.stringify(json)); } catch(err) {}
+}
+
 module.exports = {
    makeWebsocket,
+   keepalive,
+   safeSendJson,
 };
